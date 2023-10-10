1bc9f0d1ccf8fad9c1c73396562a61913df588f8
Huginode <Anonymcoder@protonmail.com>
2023-10-10 19:41:05 +0200

    Updated Code

--- Code/code.py
+++ Code/code.py
@@ -2,8 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Import Data
 data = pd.read_excel('titanic3.xls')
-data = data.drop(['name', 'sibsp', 'parch', 'ticket', 'fare', 'cabin', 'embarked', 'boat', 'body', 'home.dest'], axis = 1)
 
-a = data.describe()
+# Remove all data that isn't neede
+data = data.drop(['name', 'sibsp', 'parch','embarked', 'ticket', 'fare', 'cabin', 'boat', 'body', 'home.dest'], axis = 1)
+
+# Clean data by removing passengers with unkwon age
+data = data.dropna(axis=0)
+
+# Grouping passengers by sex and class
+a = data.groupby(['sex', 'pclass']).mean()
 print(a)
